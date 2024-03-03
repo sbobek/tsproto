a61bcf42a11f4761271319220cca9fb3d7389e07
Szymon Bobek <szymon.bobek@gmail.com>
2024-03-03 17:52:25 +0100

    further tests on imports causing docs to fail

--- src/tsproto/plots.py
+++ src/tsproto/plots.py
@@ -9,6 +9,20 @@ from ruptures.utils import pairwise
 from tsproto.utils import dominant_frequencies_for_rows
 
 
+import ruptures as rpt
+from tslearn.clustering import TimeSeriesKMeans
+from tslearn.utils import to_time_series_dataset
+from sklearn.tree import DecisionTreeClassifier
+import time
+from tslearn.clustering import KShape
+from kshape.core import KShapeClusteringCPU
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance
+from sklearn.cluster import KMeans
+from numpy import mean
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
+from sklearn.base import BaseEstimator, TransformerMixin
+import warnings
+
 def plot_and_save_barplot(target, filename, figsize=(4, 2)):
     """
     Plots a bar plot of the number of instances per class and saves it to a file.
